fe6bac3ba9340b556bf1d197ae0190c1920f0c31
karenolea <karen.oleatoledo@gmail.com>
2021-06-23 18:49:08 -0400

    se agrego quien es el ganador y titulo del documento

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,11 @@ let numeroJuegos = parseInt( prompt("Ingrese la cantidad de veces que va a jugar
 //Se crea un arreglo con las posibles respuestas de la máquina
 let respuestasMaquina = ["Piedra", "Papel", "Tijera"]
 
+//variable que cuenta las ganadas por el usuario
+let ganadasUsuario = 0;
+//variable que cuenta las ganadas por la máquina
+let ganadasMaquina = 0;
+
 //Ciclo que itera la cantidad de veces que ingresó el usuario
 for(let i = 1; i <= numeroJuegos; i++){
     //Se lee la opción ingresada por el usuario
@@ -27,30 +32,46 @@ for(let i = 1; i <= numeroJuegos; i++){
             case 'PIEDRA':
                 if(eleccionMaquina == "PAPEL"){
                     console.log("Usted Ha Perdido Contra la Máquina");
+                    ganadasMaquina++;
                 }
                 if(eleccionMaquina == "TIJERA"){
                     console.log("Felicidades! Le ha Ganado a la Máquina");
+                    ganadasUsuario++;
                 }
                 break;
             case 'PAPEL':
                 if(eleccionMaquina == "TIJERA"){
                     console.log("Usted Ha Perdido Contra la Máquina");
+                    ganadasMaquina++;
                 }
                 if(eleccionMaquina == "PIEDRA"){
                     console.log("Felicidades! Le ha Ganado a la Máquina");
+                    ganadasUsuario++;
                 }
                 break;
             case 'TIJERA':
                 if(eleccionMaquina == "PIEDRA"){
                     console.log("Usted Ha Perdido Contra la Máquina");
+                    ganadasMaquina++;
                 }
                 if(eleccionMaquina == "PAPEL"){
                     console.log("Felicidades! Le ha Ganado a la Máquina");
+                    ganadasUsuario++;
                 }
                 break;
             default:
                 console.log("No ingresaste una opción válida");
+                i--;
         }
 
     }
+}
+if(ganadasMaquina > ganadasUsuario){
+    console.log(`La máquina te ha ganado por ${ganadasMaquina} a  ${ganadasUsuario}`);
+} 
+else if(ganadasUsuario > ganadasMaquina){
+    console.log(`Le has ganado a la máquina por ${ganadasUsuario} a ${ganadasMaquina}`);
+}
+else{
+    console.log("La máquina y tú han empatado");
 }
